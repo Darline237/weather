772460bf109a4fc53de78a03eedcbbc2557a4f2a
Darline237 <darlinemakata8@gmail.com>
2023-06-20 16:42:26 +0100

    another important commit

---
+++ src/api.js
@@ -0,0 +1,26 @@
+export const geoApiOptions = {
+method: 'GET',
+headers: {
+'X-RapidAPI-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+// 'X-RapidAPI-key': 'c98f6db120msh38725063086abe6p12dd09jsnaa348bafc6c5',
+'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+
+}
+};
+
+// export const geoApiOptions = {
+//     method: 'GET',
+//     // url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/countries',
+//     headers: {
+//       'X-RapidAPI-Key': 'c98f6db120msh38725063086abe6p12dd09jsnaa348bafc6c5',
+//       'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+//     }
+//   };
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9ad1336a393d8da13d0d523979ee1570";
+// export const WEATHER_API_KEY = "6272ebcd98fcbb15bade8b8f43a7b09a";
+
+
